cdb4b72279c3aeb2d6e00d4a76e11261cbaa2052
KennedyDev454 <kennedy.fly454@gmail.com>
2025-03-24 11:16:55 -0300

    add middleware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 const express = require('express') ;
 const routes = require("./routes/routes.js");
+const errorHandler = require("./middleware/errorHandler.js");
+const cors = require("./middleware/cors.js");
 require('express-async-errors');
 const app = express();
 app.use(express.json());
-
+app.use(cors);
 app.use(routes);
-app.use((err,req,res) => {
-    console.log(err);
-    res.sendStatus(500);
-});
-
-
+app.use(errorHandler);
 
 app.listen(5000,() => console.log("Server is running on port 5000..."));
 module.exports =  app;
---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,4 @@
+module.exports = (err, req, res) => {
+    console.log(err);
+    res.sendStatus(500);
+}
